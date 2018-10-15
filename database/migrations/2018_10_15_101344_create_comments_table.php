e0dee554b60f7becb0726c157c695468851b7bea
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2018-10-15 12:26:21 +0200

    Eerste commit

---
+++ database/migrations/2018_10_15_101344_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('author');
+            $table->longText('contents');
+
+            // Link naar articles
+            $table->unsignedInteger('article_id');
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}

7ab3c2fb7be972f92997f0e445445952192b5980
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2018-10-24 19:24:53 +0200

    nog eens een commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <script src="js/app.js"></script>
 
     <!-- JQuery and poppler -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
